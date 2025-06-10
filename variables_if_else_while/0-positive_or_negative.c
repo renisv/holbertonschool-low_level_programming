454f9a0e8a466196d19eb7b3133f4821275d737a
renisv <11432@holbertonstudents.com>
2025-06-10 09:42:27 +0000

    update

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
