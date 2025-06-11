aa0380866fe229589d7e7a0c55219d2d6c580798
renisv <11432@holbertonstudents.com>
2025-06-11 08:17:30 +0000

    update

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,16 +8,15 @@
 
 int main(void)
 {
-    char c;
-    
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        if (c != 'e' && c != 'q')
-        {
-            putchar(c);
-        }
-    }
-    putchar('\n');
-    
-    return (0);
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+}
+putchar('\n');
+return (0);
 }
