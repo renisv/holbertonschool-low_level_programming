8b890a85cfb645617deb89dd822d65d118f05bca
renisv <11432@holbertonstudents.com>
2025-06-11 08:05:31 +0000

    update

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <=9; i++)
+	{
+		putchar(i+'0');
+	}
+	putchar('\n');
+
+	return (0);
+}
