5d25495577e109aeecc61c286116f1be4d920fca
renisv <11432@holbertonstudents.com>
2025-06-13 08:16:11 +0000

    update

--- functions_nested_loops/7-main.c
+++ functions_nested_loops/7-main.c
@@ -11,11 +11,8 @@ int main(void)
 {
     int n;
 	print_last_digit(98);
-    _putchar('\n');
 	print_last_digit(0);
-    _putchar('\n');
 	n = print_last_digit(-1024);
-    _putchar('\n');
 	_putchar('0' + n);
 	_putchar('\n');
     
