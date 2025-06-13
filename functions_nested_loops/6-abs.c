0821e4724d16dfaf21688867ba10343c0c677a51
renisv <11432@holbertonstudents.com>
2025-06-13 08:08:54 +0000

    update

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,13 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+* _abs - checks value of number
+* @num: parameter to hold value
+* Return: num or -num
+*/
 int _abs(int num)
-{ 
-    if (num >= 0) 
-    {
-        return num;
-    }
-    else {
-        return -num;
-    }
+{
+if num >= 0
+{
+return (num);
+}
+else
+{
+return (-num);
+}
 }
