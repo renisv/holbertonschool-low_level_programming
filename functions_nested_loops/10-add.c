5f7db02841684d439825a5612b0960b7b96c84c0
renisv <11432@holbertonstudents.com>
2025-06-14 13:50:30 +0000

    update

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* add - prints the time table
+* @int1: parameter first num
+* @int2: parameter second num
+* Return: time table
+*/
+
+int add(int int1, int int2)
+{
+	int sum;
+	sum = int1 + int2;
+
+	if (sum <= 9)
+	_putchar(sum);
+	else
+	_putchar ((sum / 10) + '0');
+	_putchar((sum % 10) + '0');
+	_putchar('\n');
+return (sum);
+}
---
+++ functions_nested_loops/10-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    add(3, 5);
+    return (0);
+}
