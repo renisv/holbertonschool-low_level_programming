37201d610e78146a11b1e4be06ef6cb87b4f36b0
renisv <11432@holbertonstudents.com>
2025-06-14 15:21:10 +0000

    update

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+* print_to_98 - prints to 98
+* @n : parameter to hold value
+* Return: full numbers to 98
+*/
+
 void print_to_98(int n)
 {
 	if (n >= 98)
