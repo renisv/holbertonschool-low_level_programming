afb6d4f47e9fd3283775f9a6eca075f2623eeee4
renisv <11432@holbertonstudents.com>
2025-06-16 11:29:48 +0000

    update

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+* print_line - prints a line
+* @n: parameter to hold value
+* Return: start
+*/
+
 void print_line(int n)
 {
 	int start = 0;
+
 	while (start < n)
 	{
 		_putchar('_');
