bf74074b471e5f7c281fba3f3824be30a9dcde88
renisv <11432@holbertonstudents.com>
2025-06-16 11:52:23 +0000

    update

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
+int _putchar(char c);
 void print_diagonal(int n)
 {
+	int j;
+	int i;
+
+	for (i = 0; i <= n ; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+		_putchar(' ');
+		}
 	
+	_putchar('\\');
+	_putchar('\n');
+	}
 }
