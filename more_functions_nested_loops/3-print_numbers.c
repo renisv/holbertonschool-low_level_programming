a940e69c4fc6bb32669ee529389a10de0f689c97
renisv <11432@holbertonstudents.com>
2025-06-16 11:24:49 +0000

    update

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
+/**
+* print_numbers - prints numbers
+* Return: i = '0'
+*/
+
 void print_numbers(void)
 {
 	int i;
-	for (i=0; i <= 9; i++)
+
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i + '0');
 	}
 		_putchar('\n');
-	
 }
