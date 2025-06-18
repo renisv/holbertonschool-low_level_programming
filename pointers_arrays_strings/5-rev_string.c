fe38386d362eca1ccb8342fa70c1d7a6583e150b
renisv <11432@holbertonstudents.com>
2025-06-18 09:06:19 +0000

    update

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,3 +1,18 @@
 #include "main.h"
 
 void rev_string(char *s)
+{
+    int length, l_idx, tmp;
+    for (length = 0; s[length] != 0; length++)
+    {
+
+    }
+    l_idx = length - 1;
+    while (l_idx >= length / 2)
+    {
+        tmp = s[l_idx];
+        s[l_idx] = s[length - 1 - l_idx];
+        s[length - 1 - l_idx] = tmp;
+        l_idx--;
+    }
+}
