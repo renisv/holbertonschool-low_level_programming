f6035a332871fa90158f8f07045e83610261f56a
renisv <11432@holbertonstudents.com>
2025-06-19 09:14:45 +0000

    update

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
+
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 
 	}
-	for (j = 0; src[j] != '\0'; j++)
+	for (j = 0; src[j] == src[n]; j++)
 	{
 		dest[i + j] = src[j];
 	}
-	dest[i +j] = '\0';
+	dest[i + j] = '\0';
 
 	return (dest);
 }
