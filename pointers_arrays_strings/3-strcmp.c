d96345e778f115ae15cfe1359a98ff53d51bc03b
renisv <11432@holbertonstudents.com>
2025-06-19 10:50:49 +0000

    update

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+    
+    for (i = 0; s1[i] && s2[i]; i++)
+    {
+        if (s1[i] != s2[i])
+            return s1[i] - s2[i];
+    }
+    
+    return s1[i] - s2[i];
+}
