76b620e74624a3412a9e69365f6121ab6d8f85c2
renisv <11432@holbertonstudents.com>
2025-06-19 11:31:38 +0000

    update

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+char *cap_string(char *str)
+{
+	char separators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ( i == 0)
+		{
+			
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i] == separators[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] = str[i + 1] - 32;
+				}
+			}
+		}
+	}
+	
+	return(str);
+}
