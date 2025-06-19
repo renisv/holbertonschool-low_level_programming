3706942a15462f4319a4f226fb401c3d6da058eb
renisv <11432@holbertonstudents.com>
2025-06-19 11:08:13 +0000

    update

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i <= n / 2 - 1; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+	}
+}
