a3385bed41c034ad82d7eb5a8d1ab5fbfffa066f
renisv <11432@holbertonstudents.com>
2025-06-26 09:35:56 +0000

    update

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -2,33 +2,30 @@
 
 /**
  * _strstr - locates a substring
- * @haystack: string to search through
- * @needle: substring to find
- *
- * Description: Finds the first occurrence of the substring 'needle'
- *              in the string 'haystack'. The terminating null bytes
- *              are not compared.
+ * @haystack: the string to search in
+ * @needle: the substring to search for
  *
  * Return: pointer to the beginning of the located substring,
- *         or NULL if the substring is not found
+ * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-unsigned int i, j;
+	int i, j;
 
-if (*needle == '\0')
-return (haystack);
+	if (*needle == '\0')
+		return (haystack);
 
-for (i = 0; haystack[i] != '\0'; i++)
-{
-for (j = 0; needle[j] != '\0'; j++)
-{
-if (haystack[i + j] != needle[j])
-break;
-}
-if (needle[j] == '\0')
-return (&haystack[i]);
-}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
 
-return (NULL);
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
+
+	return (0);
 }
