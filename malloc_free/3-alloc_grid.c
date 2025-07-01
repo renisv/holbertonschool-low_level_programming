38ddd3dc757cd33de901c2f75791a9753af58e79
renisv <11432@holbertonstudents.com>
2025-07-01 10:30:12 +0000

    update

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,8 +14,7 @@ int **alloc_grid(int width, int height)
         return (NULL);
     for (i = 0; i < height; i++)
     {
-        grid[i] = malloc(sizeof(int*
-        ) * width);
+        grid[i] = malloc(sizeof(int) * width);
         if (grid[i] == NULL)
         {
             for (j = 0; j < i; j++)
