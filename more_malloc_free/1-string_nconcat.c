188318a4f3bf057cbc6d4c6cba0e64307e5f973b
renisv <11432@holbertonstudents.com>
2025-07-02 09:02:41 +0000

    update

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,6 +7,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     unsigned int s1_len, s2_len, i, j;
     char *concat;
 
+    if (s1 == NULL)
+    {
+        s1 = "";
+    }
+
+    if (s2 == NULL)
+    {
+        s2 = "";
+    }
+
     for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
     {
 
