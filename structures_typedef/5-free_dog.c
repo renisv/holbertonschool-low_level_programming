47941d7b4b8fc94871191ae3bf8c411034a9945a
renisv <11432@holbertonstudents.com>
2025-07-03 10:36:40 +0000

    update

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+void free_dog(dog_t *d)
+{
+    if (d != NULL)
+    
+    {
+        free(d -> name);
+        free(d -> owner);
+        free(d);
+    }
+}
