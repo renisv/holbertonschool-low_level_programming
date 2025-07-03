4c3626c7e7c5ec2246973bb541cdc01e0f8b3196
renisv <11432@holbertonstudents.com>
2025-07-03 17:20:22 +0000

    update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Structure representing a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 typedef struct dog dog_t;
