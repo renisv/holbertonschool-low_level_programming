ebdf4b50f9ef26ecce1636f4b1d78f477d3f41b6
renisv <11432@holbertonstudents.com>
2025-07-07 08:56:42 +0000

    update

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-void name_print(char *example)
-{
-    printf("%s",example);
-}
-
 void print_name(char *name, void (*f)(char *))
 {
     f(name);
