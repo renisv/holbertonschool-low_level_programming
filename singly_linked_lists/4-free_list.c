6942e5cf96c2b04d30e0fd96f7d9af912b1370a1
renisv <11432@holbertonstudents.com>
2025-07-21 10:45:56 +0000

    update

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
