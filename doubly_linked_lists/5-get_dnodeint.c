fb029740f3336f2a1938526c2f32eecb7b3c507d
renisv <11432@holbertonstudents.com>
2025-07-22 10:26:13 +0000

    update

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -9,10 +9,15 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	count = 0;
 
-	while (count != index)
+	if (head == NULL)
+		return (NULL);
+	
+	while (head != NULL)
 	{
-		head = head->next;
+		if (index == count)
+			return (head);
 		count++;
+		head = head->next;
 	}
 
 	return (head);
