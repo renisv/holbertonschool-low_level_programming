363edca7751de0da0434965ec738f7d999e379ca
renisv <11432@holbertonstudents.com>
2025-07-22 08:20:56 +0000

    update

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
