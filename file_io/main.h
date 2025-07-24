4c27ae536f8d1785f8e266585996fcaf99438c30
renisv <11432@holbertonstudents.com>
2025-07-24 10:18:33 +0000

    update

--- file_io/main.h
+++ file_io/main.h
@@ -17,6 +17,7 @@ int create_file(const char *filename, char *text_content);
 /* 2-append_text_to_file.c */
 int append_text_to_file(const char *filename, char *text_content);
 
-
+void cp(char *file_from, char *file_to);
+void cls(int file);
 
 #endif /* MAIN_H */
