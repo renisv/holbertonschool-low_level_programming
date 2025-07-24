2855306b50af834edc64a69689a34cac982d9cf5
renisv <11432@holbertonstudents.com>
2025-07-24 08:51:26 +0000

    update

---
+++ file_io/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* 0-read_textfile.c */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* 1-create_file.c */
+int create_file(const char *filename, char *text_content);
+
+/* 2-append_text_to_file.c */
+int append_text_to_file(const char *filename, char *text_content);
+
+/* 3-cp.c */
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */
