68607a4cac5cd828949cca37df4363f8550ca3fa
renisv <11432@holbertonstudents.com>
2025-07-24 08:56:36 +0000

    update

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -6,7 +6,7 @@
 int sum_dlistint(dlistint_t *head)
 {
 	int sum;
-	
+
 	sum = 0;
 
 	while (head != NULL)
@@ -14,6 +14,6 @@ int sum_dlistint(dlistint_t *head)
 		sum = sum + head->n;
 		head = head->next;
 	}
-	
+
 	return (sum);
 }
