675bda21a2cb252911cf6f20322c2310c6b85680
renisv <11432@holbertonstudents.com>
2025-07-24 09:25:08 +0000

    update

--- file_io/main.h
+++ file_io/main.h
@@ -17,7 +17,6 @@ int create_file(const char *filename, char *text_content);
 /* 2-append_text_to_file.c */
 int append_text_to_file(const char *filename, char *text_content);
 
-/* 3-cp.c */
-int main(int argc, char *argv[]);
+
 
 #endif /* MAIN_H */
