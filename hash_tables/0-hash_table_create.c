3ae98221e6868e94f02c635dcc5eff22a7ae5571
renisv <11432@holbertonstudents.com>
2025-08-06 09:32:55 +0000

    update

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	
+	if (table == NULL)
+		return (NULL);
+	
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+
+	if (table->array == NULL)
+		return(NULL);
+	
+	return(table);
+}
