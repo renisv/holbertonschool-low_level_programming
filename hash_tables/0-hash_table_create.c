f3e060ab66c86f736506f39b6450e1ef7bf01b7e
renisv <11432@holbertonstudents.com>
2025-08-07 10:36:08 +0000

    update

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,18 +3,30 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - Creates a new hash table
+ * @size: The size of the array
+ *
+ * Description:
+ * Allocates memory for a hash table structure and its array.
+ * Initializes each element of the array to NULL.
+ *
+ * Return:
+ * Pointer to the newly created hash table, or NULL if allocation fails
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
-	
+
 	if (table == NULL)
 		return (NULL);
-	
+
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t*) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (table->array == NULL)
-		return(NULL);
-	
-	return(table);
+		return (NULL);
+
+	return (table);
 }
+
--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -3,6 +3,19 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+/**
+ * key_index - Generates the index for a key in a hash table
+ * @key: The key string to hash
+ * @size: The size of the hash table array
+ *
+ * Description:
+ * Uses the djb2 hash function to generate a hash value,
+ * then calculates the index by taking the modulo of the hash
+ * with the size of the array.
+ *
+ * Return:
+ * The computed index for the key in the hash table
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
